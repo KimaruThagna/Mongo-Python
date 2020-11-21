7149154e6016001742b5549302f639de07760d2a
Kimaru thagana <macbuntu@localhost.localdomain>
2020-11-21 21:58:15 +0300

    multiple pymongo functions

--- db_config.py
+++ db_config.py
@@ -2,6 +2,13 @@ from pymongo import MongoClient
 
 client = MongoClient('mongodb://localhost:27017/')
 db = client['mongo-python']
+# list collections
+#db.list_collection_names()
 print("Database created........")
 collection = db['car_locations']
+# drop collections
+#collection.drop()
+
+# issue commands
+#result = db.command("serverStatus") or dbstats
 print("Colletion created........")
--- mongo_python_scripts.py
+++ mongo_python_scripts.py
@@ -1,5 +1,5 @@
 from db_config import client, collection
-
+from pymongo import DESCENDING,ASCENDING
 cars = [ {'name': 'Audi', 'price': 52642},
     {'name': 'Mercedes', 'price': 57127},
     {'name': 'Skoda', 'price': 9000},
@@ -9,6 +9,31 @@ cars = [ {'name': 'Audi', 'price': 52642},
     {'name': 'Hummer', 'price': 41400},
     {'name': 'Volkswagen', 'price': 21600} ]
 
+
 def multiple_data_insert():
     collection.insert_many(cars)
     return 'Success. multiple inserts committed'
+
+def multiple_data_retrieve():
+    return collection.find()
+
+def count_retrieved_data():
+    return collection.find().count()
+
+def expensive_cars(value):
+    return collection.find({'price':{'$gt':value}})
+
+def cheap_cars(value):
+    return collection.find({'price':{'$lt':value}})
+#pymongo projections
+def select_subfields():
+    return collection.find({},{'_id':1,'name':1})
+
+def sort_documents(bool):
+    if bool:
+        return collection.sort('price',DESCENDING)
+    else:
+        return collection.sort('price',ASCENDING)
+
+def collection_limit_output():
+    return collection.find().skip(2).limit(3) # skip the first 2 documents, limit results to 3 documents
