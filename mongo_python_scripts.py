ed8998e48862b003e7e3e2f7fdf1eb2179014dd0
Kimaru thagana <macbuntu@localhost.localdomain>
2020-11-22 09:46:37 +0300

    update functions

--- mongo_python_scripts.py
+++ mongo_python_scripts.py
@@ -1,5 +1,5 @@
 from db_config import client, collection
-from pymongo import DESCENDING,ASCENDING
+from pymongo import DESCENDING,ASCENDING, ReturnDocument
 cars = [ {'name': 'Audi', 'price': 52642},
     {'name': 'Mercedes', 'price': 57127},
     {'name': 'Skoda', 'price': 9000},
@@ -13,9 +13,35 @@ cars = [ {'name': 'Audi', 'price': 52642},
 def multiple_data_insert():
     collection.insert_many(cars)
     return 'Success. multiple inserts committed'
-
+# db.collection.find_one() retrieves the first document in the  collection
 def multiple_data_retrieve():
     return collection.find()
+
+def single_retrieve(id):
+    return collection.find({"_id":id})
+
+def find_and_update(id):
+    collection.find_one_and_update({'_id':id},
+                                   {'$inc':{'price':0.554}},
+                                   return_document=ReturnDocument.AFTER) # return the updated version of a document
+def single_update(id):
+    collection.update({'_id':id},{'$inc':{"price":25.55},
+                                  '$set':{"name":'Fancy new car'}
+                                  })
+# each appends multiple values in the array field
+def update_with_array(id):
+    collection.update({'_id':id},
+                      {'$push':
+                           { 'extras':{'$each':['sun roof', 'driver partition','keyless start'],'$sort':ASCENDING}
+                           }
+                       })
+
+def pop_from_array(id):
+    collection.update({'_id':id}, {'$pop':{'extras':-1 # remove the first element from array
+                                           }
+                                   })
+def update_or_insert(id):
+    pass
 
 def count_retrieved_data():
     return collection.find().count()
